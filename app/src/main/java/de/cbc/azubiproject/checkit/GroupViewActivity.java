73cee3ca7184c5c16b101679e33256cfb3681ea3
Anheier <tmtipps-help@gmx.de>
2018-07-25 16:34:22 +0200

    new activities added.

---
+++ app/src/main/java/de/cbc/azubiproject/checkit/GroupViewActivity.java
@@ -0,0 +1,61 @@
+package de.cbc.azubiproject.checkit;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Toast;
+
+public class GroupViewActivity extends AppCompatActivity {
+
+    private boolean pressedOnce;
+    private boolean loggedIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // set login state
+        loggedIn = getIntent().getBooleanExtra(LoginActivity.LOGIN_STATE, false);
+
+        if (loggedIn) {
+            setContentView(R.layout.activity_group_view);
+        } else {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == event.KEYCODE_BACK) {
+            if (!pressedOnce) {
+                pressedOnce = true;
+                Toast.makeText(this, "Erneut drücken, um die App zu beenden.", Toast.LENGTH_LONG).show();
+
+                // Führt erst innerhalb des Intervals die Runnable-Klasse als Callback aus
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        pressedOnce = false;
+                    }
+                }, 3000);
+            } else {
+                pressedOnce = false;
+                onBackPressed();
+            }
+
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public void btnGroup_onClick(View view)
+    {
+        Intent intent = new Intent(this, GroupActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/de/cbc/azubiproject/checkit/LoginActivity.java
+++ app/src/main/java/de/cbc/azubiproject/checkit/LoginActivity.java
@@ -1,5 +1,6 @@
 package de.cbc.azubiproject.checkit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,25 +15,36 @@ public class LoginActivity extends AppCompatActivity {
     private Button btnLogin;
     private EditText editTextUsername;
     private EditText editTextPassword;
+    private boolean loggedIn = true;
+    public static final String LOGIN_STATE = "de.cbc.checkit.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        if (loggedIn) {
+            Intent intent = new Intent(this, GroupViewActivity.class);
+            intent.putExtra(LOGIN_STATE, true);
+            startActivity(intent);
+        }
+    }
+
+    public void btnLogin_onClick(View view)
+    {
         btnLogin = findViewById(R.id.btnLogin);
         editTextUsername = findViewById(R.id.editTextUsername);
         editTextPassword = findViewById(R.id.editTextPassword);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (editTextUsername.getText().toString().equals("foo") && editTextPassword.getText().toString().equals("bar")) {
-                    Toast.makeText(getApplicationContext(), "Erfolgreich eingeloggt.", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "Login leider nicht erfolgreich.", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
+        if (editTextUsername.getText().toString().equals("foo") && editTextPassword.getText().toString().equals("bar")) {
+            Toast.makeText(getApplicationContext(), "Erfolgreich eingeloggt.", Toast.LENGTH_LONG).show();
+
+            // redirect to activity
+            Intent intent = new Intent(this, GroupViewActivity.class);
+            intent.putExtra(LOGIN_STATE, true);
+            startActivity(intent);
+        } else {
+            Toast.makeText(getApplicationContext(), "Login leider nicht erfolgreich.", Toast.LENGTH_LONG).show();
+        }
     }
 }
