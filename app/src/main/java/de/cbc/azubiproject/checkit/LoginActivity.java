bcec701288ea41ae75b7ab6cb1cdf1578d74191e
Anheier <tmtipps-help@gmx.de>
2018-08-01 13:24:42 +0200

    - new images added - login and register screen layout changed

--- app/src/main/java/de/cbc/azubiproject/checkit/LoginActivity.java
+++ app/src/main/java/de/cbc/azubiproject/checkit/LoginActivity.java
@@ -1,11 +1,13 @@
 package de.cbc.azubiproject.checkit;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
@@ -13,13 +15,18 @@ public class LoginActivity extends AppCompatActivity {
     private Button btnLogin;
     private EditText editTextUsername;
     private EditText editTextPassword;
-    private boolean loggedIn = true;
+    private boolean loggedIn = false;
+    private TextView forgetPassword;
     public static final String LOGIN_STATE = "de.cbc.checkit.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        forgetPassword = (TextView) findViewById(R.id.textViewForgetPassword);
+        Typeface ubuntu = Typeface.createFromAsset(getAssets(), "font/Ubuntu-R.ttf");
+        forgetPassword.setTypeface(ubuntu);
 
         if (loggedIn) {
             Intent intent = new Intent(this, GroupViewActivity.class);
@@ -31,7 +38,7 @@ public class LoginActivity extends AppCompatActivity {
     public void btnLogin_onClick(View view)
     {
         btnLogin = findViewById(R.id.btnLogin);
-        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextUsername = findViewById(R.id.editTextBirthdate);
         editTextPassword = findViewById(R.id.editTextPassword);
 
         if (editTextUsername.getText().toString().equals("foo") && editTextPassword.getText().toString().equals("bar")) {
@@ -44,5 +51,11 @@ public class LoginActivity extends AppCompatActivity {
         } else {
             Toast.makeText(getApplicationContext(), "Login leider nicht erfolgreich.", Toast.LENGTH_LONG).show();
         }
+    }
+
+    public void btnRegister_onClick(View view)
+    {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
