82737b9890046f68d98ec2888664a2dacbb8b7b0
Til Anheier <tmtipps-help@gmx.de>
2018-08-04 19:49:11 +0200

    - logic in profile activity added

--- app/src/main/java/de/cbc/azubiproject/checkit/ProfileActivity.java
+++ app/src/main/java/de/cbc/azubiproject/checkit/ProfileActivity.java
@@ -7,10 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.concurrent.ExecutionException;
+
+import de.cbc.azubiproject.containers.RepositoryContainer;
+import de.cbc.azubiproject.facades.GroupFacade;
 import de.cbc.azubiproject.models.ActionBarHelper;
+import de.cbc.azubiproject.models.User;
 
 public class ProfileActivity extends ActionBarHelper {
-    private TextView textViewUsername, textViewAskedQuestions, textViewAskedQuestionsSince, textViewRegisteredSince, textViewRegisteredSinceString, textViewActiveGroups;
+    private TextView textViewUsername, textViewAskedQuestions, textViewAskedQuestionsString, textViewRegisteredSince, textViewRegisteredSinceString, textViewActiveGroups;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +26,30 @@ public class ProfileActivity extends ActionBarHelper {
 
         textViewUsername = (TextView) findViewById(R.id.textViewUsername);
         textViewAskedQuestions = (TextView) findViewById(R.id.textViewAskedQuestions);
-        textViewAskedQuestionsSince = (TextView) findViewById(R.id.textViewAskedQuestionsString);
+        textViewAskedQuestionsString = (TextView) findViewById(R.id.textViewAskedQuestionsString);
         textViewRegisteredSince = (TextView) findViewById(R.id.textViewRegisteredSince);
         textViewRegisteredSinceString = (TextView) findViewById(R.id.textViewRegisteredSinceString);
         textViewActiveGroups = (TextView) findViewById(R.id.textViewActiveGroups);
 
         textViewUsername.setTypeface(ubuntu);
         textViewAskedQuestions.setTypeface(ubuntu);
-        textViewAskedQuestionsSince.setTypeface(ubuntu);
+        textViewAskedQuestionsString.setTypeface(ubuntu);
         textViewRegisteredSince.setTypeface(ubuntu);
         textViewRegisteredSinceString.setTypeface(ubuntu);
         textViewActiveGroups.setTypeface(ubuntu);
+
+        try {
+            // load user data
+            RepositoryContainer repositoryContainer = new GroupFacade().getRepositories();
+            User user = repositoryContainer.getUserGroupCollection().getUserRepository().getByUsername("anheier");
+
+            // set profile
+            textViewUsername.setText(user.getProfile().getFirstName());
+            textViewRegisteredSinceString.setText(user.getRegisterDate());
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
