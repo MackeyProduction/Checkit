7f4eac7c7a9ca063a8842d2f2cc47ac0735415d4
Anheier <tmtipps-help@gmx.de>
2018-08-15 16:46:27 +0200

    - regex implemented

--- app/src/main/java/de/cbc/azubiproject/asynctasks/AddUserGroupTask.java
+++ app/src/main/java/de/cbc/azubiproject/asynctasks/AddUserGroupTask.java
@@ -2,6 +2,9 @@ package de.cbc.azubiproject.asynctasks;
 
 import android.os.AsyncTask;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import de.cbc.azubiproject.http.Endpoint;
 import de.cbc.azubiproject.http.HttpRequest;
 import de.cbc.azubiproject.http.HttpResponse;
@@ -9,6 +12,15 @@ import de.cbc.azubiproject.http.HttpResponse;
 public class AddUserGroupTask extends AsyncTask<String, Void, HttpResponse> {
     @Override
     protected HttpResponse doInBackground(String... strings) {
+        String pattern = "(\\\"|\\'|\\\\|\\$)+";
+
+        Pattern regEx = Pattern.compile(pattern);
+
+        Matcher matchGroupName = regEx.matcher(strings[1]);
+
+        if (matchGroupName.find()) {
+            strings[1] = strings[1].replaceAll(pattern, "");
+        }
         String json = String.format("{\"username\":\"%s\",\"groupName\":\"%s\",\"created\":\"%s\"}", strings[0], strings[1], strings[2]);
         return new HttpRequest(new Endpoint("/userGroups.php")).postRequest(json);
     }
